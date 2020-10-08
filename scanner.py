ac2b074627f3bfbb1e6d31ac96b7742bdb6b3b08
Arghavan Rezvani <a.rezvani99@yahoo.com>
2020-10-08 21:47:26 +0330

    keyword_identifier dfa

--- scanner.py
+++ scanner.py
@@ -18,7 +18,22 @@ def num_dfa(pointer, line):
 
 
 def keyword_identifier_dfa(pointer, line):
-    pass
+    lexeme = line[pointer]
+    other = WHITE_SPACE.union(SYMBOL).union('/')
+    while True:
+        pointer += 1
+        cur_char = line[pointer]
+        if cur_char in DIGIT.union(LETTER):
+            lexeme += cur_char
+        elif cur_char in other:
+            if lexeme in KEYWORDS:
+                type_of_token = 'keyword'
+            else:
+                type_of_token = 'identifier'
+            return pointer, lexeme, type_of_token
+        else:
+            lexeme += cur_char
+            raise PanicException(pointer + 1, lexeme, 'Invalid input')
 
 
 def eq_symbol_dfa(pointer, line):
@@ -38,4 +53,7 @@ def comment_dfa(pointer, line):
 
 
 def whitespace_dfa(pointer, line):
-    pass
+    lexeme = line[pointer]
+    type_of_token = 'whitespace'
+    pointer += 1
+    return pointer, lexeme, type_of_token

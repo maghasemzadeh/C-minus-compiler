64ca5a9d28b0d0f4afa46e871a61dd4e13be307b
Arghavan Rezvani <a.rezvani99@yahoo.com>
2020-10-08 13:12:14 +0330

    dfa first step

---
+++ asset.py
@@ -0,0 +1,7 @@
+import string
+
+LETTER = set(string.ascii_letters)
+DIGIT = set(string.digits)
+WHITE_SPACE = set(string.whitespace)
+SYMBOL = {';', ':', ',', '[', ']', '(', ')', '{', '}', '+', '-', '*', '=', '<', '=='}
+KEYWORDS = {'if', 'else', 'void', 'int', 'while', 'break', 'switch', 'default', 'case', 'return'}
---
+++ scanner.py
@@ -0,0 +1,26 @@
+def num_dfa(pointer, line):
+    pass
+
+
+def keyword_identifier_dfa(pointer, line):
+    pass
+
+
+def eq_symbol_dfa(pointer, line):
+    pass
+
+
+def star_symbol_dfa(pointer, line):
+    pass
+
+
+def symbol_dfa(pointer, line):
+    pass
+
+
+def comment_dfa(pointer, line):
+    pass
+
+
+def whitespace_dfa(pointer, line):
+    pass

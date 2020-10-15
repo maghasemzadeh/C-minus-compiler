12a09214e9b623dcfe2d51b6f814d9555d418140
amin <m.agasemzadeh@gmail.com>
2020-10-15 20:35:55 +0330

    add run_test.sh script to run tests automaticaly

--- compiler.py
+++ compiler.py
@@ -8,9 +8,10 @@ def write_output_to_file(total_tokens):
         if not line:
             total_tokens[i] = ''
             continue
-        output_tokens = str(i + 1) + '.\t\t'
+        output_tokens = str(i + 1) + '.\t'
         for token in line:
             output_tokens += str(token) + ' '
+        output_tokens = output_tokens[:-1]
         total_tokens[i] = output_tokens.replace("'", '') + '\n'
     with open('tokens.txt', 'w') as output_file:
         print('helloooo')
@@ -23,9 +24,10 @@ def write_errors_to_file(lexical_errors):
     else:
         lexical_errors = [lexical_errors[k] for k in sorted(lexical_errors)]
         for i, item in enumerate(lexical_errors):
-            error = str(item[0][0]) + '.\t\t'
+            error = str(item[0][0]) + '.\t'
             for (line, lexeme, message) in item:
                 error += f'({lexeme}, {message}) '
+            error = error[:-1]
             lexical_errors[i] = error + '\n'
     with open('lexical_errors.txt', 'w') as error_file:
         error_file.writelines(lexical_errors)
@@ -34,7 +36,7 @@ def write_errors_to_file(lexical_errors):
 def write_symbols_to_file(symbol_table):
     symbols_string = []
     for i, symbol in enumerate(symbol_table):
-        symbol_line = f'{i + 1}.\t\t{symbol}'
+        symbol_line = f'{i + 1}.\t{symbol}'
         symbols_string.append(symbol_line + '\n')
     with open('symbol_table.txt', 'w') as symbol_file:
         symbol_file.writelines(symbols_string)

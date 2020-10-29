eab2e57e094b7b6e9a2ec8a1cdb0cf5d6134e0b9
amin <m.agasemzadeh@gmail.com>
2020-10-29 22:15:52 +0330

    change assests.py name

---
+++ assets.py
@@ -0,0 +1,11 @@
+import string
+
+LETTER = set(string.ascii_letters)
+DIGIT = set(string.digits)
+WHITE_SPACE = set(string.whitespace)
+SYMBOL = {';', ':', ',', '[', ']', '(', ')', '{', '}', '+', '-', '*', '=', '<'}
+KEYWORDS = {'if', 'else', 'void', 'int', 'while', 'break', 'switch', 'default', 'case', 'return'}
+VALID_CHARS = LETTER.union(DIGIT).union(WHITE_SPACE).union(SYMBOL)
+
+EPSILON = 'ε'
+SYNCH = 'synch'

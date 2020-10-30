2b0f7425fc0edede0384771d60351ede2a972528
arghavan99 <55342129rez>
2020-10-30 16:37:44 +0330

    do not return whitespace tokens

--- parser.py
+++ parser.py
@@ -26,7 +26,11 @@ class Parser:
         advance_input = True
         while True:
             if advance_input:
-                lookahead, token_type, line_no = self.scanner.get_next_token()
+                lexeme, token_type, line_no = self.scanner.get_next_token()
+                if token_type in ['KEYWORD', 'SYMBOL']:
+                    lookahead = lexeme
+                else:
+                    lookahead = token_type
             stack_top = self.stack[-1]
             if stack_top in self.non_terminals:
                 rules = self.next_state(stack_top, lookahead)
@@ -44,7 +48,7 @@ class Parser:
                     if self.indexes_stack and stack_len < self.indexes_stack[-1]:
                         self.last_node = self.node_stack.pop(-1)
                         self.indexes_stack.pop(-1)
-                    self.stack.pop(-1) 
+                    self.stack.pop(-1)
                     for r in range(len(rules), -1, -1):
                         rule = rules[r]
                         if rule in self.non_terminals:

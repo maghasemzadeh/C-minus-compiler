05bd8dcd93dafd386dab514107a85eff7423a9bb
amin <m.agasemzadeh@gmail.com>
2020-11-07 11:00:37 +0330

    change first follow grammer cases

--- run_test.sh
+++ run_test.sh
@@ -2,18 +2,15 @@
 
 echo "" > log.txt
 echo "" > brief_results.txt
-for dir in ./PA1_input_output_samples/*; do
+for dir in ./PA2_Resources/*; do
     cp "${dir}/input.txt" input.txt 
     python compiler.py
     printf "\n\n\n\n=====================================>>>>> Running Test ${dir}...\n" >> log.txt
     printf "\n\n=====================================>>>>> Running Test ${dir}...\n" >> brief_results.txt
-    printf "\n\n              *** tokens.txt diffrences ***\n" >> log.txt
-    diff tokens.txt "${dir}/tokens.txt" >> log.txt
-    diff -q tokens.txt "${dir}/tokens.txt" >> brief_results.txt
-    printf "\n\n              *** lexical_errors.txt diffrences ***\n" >> log.txt
-    diff lexical_errors.txt "${dir}/lexical_errors.txt" >> log.txt
-    diff -q lexical_errors.txt "${dir}/lexical_errors.txt" >> brief_results.txt
-    printf "\n\n              *** symbol_table.txt diffrences ***\n" >> log.txt
-    diff symbol_table.txt "${dir}/symbol_table.txt" >> log.txt
-    diff -q symbol_table.txt "${dir}/symbol_table.txt" >> brief_results.txt
+    printf "\n\n              *** parse_tree.txt diffrences ***\n" >> log.txt
+    diff parse_tree.txt "${dir}/parse_tree.txt" >> log.txt
+    diff -q parse_tree.txt "${dir}/parse_tree.txt" >> brief_results.txt
+    printf "\n\n              *** syntax_errors.txt diffrences ***\n" >> log.txt
+    diff syntax_errors.txt "${dir}/syntax_errors.txt" >> log.txt
+    diff -q syntax_errors.txt "${dir}/syntax_errors.txt" >> brief_results.txt
 done
